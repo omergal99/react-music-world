c664624f41370c25cafe4d56e817b15047f70b81
omergal99 <omergal99gm@gmail.com>
2019-08-17 17:46:44 +0300

    Updates

---
+++ precache-manifest.2a81d8c3a88a220e8a7582d1491641ac.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65c9b6620c67f16287bc71d334d1e55",
+    "url": "/react-music-world/index.html"
+  },
+  {
+    "revision": "e540ddcdfcc13e12fc65",
+    "url": "/react-music-world/static/css/main.4ae1352b.chunk.css"
+  },
+  {
+    "revision": "4b3cde3768457b7f73d7",
+    "url": "/react-music-world/static/js/2.ec95753a.chunk.js"
+  },
+  {
+    "revision": "e540ddcdfcc13e12fc65",
+    "url": "/react-music-world/static/js/main.f708d5de.chunk.js"
+  },
+  {
+    "revision": "ffbcd5a7e025b5e7edbd",
+    "url": "/react-music-world/static/js/runtime~main.b7fc2ecd.js"
+  },
+  {
+    "revision": "2106f2b5c35ac033c99b2d869294ba08",
+    "url": "/react-music-world/static/media/Beautiful_Memories.2106f2b5.mp3"
+  },
+  {
+    "revision": "5b81481392cce7e10f82250ed055bbb7",
+    "url": "/react-music-world/static/media/Bruno Mars - Runaway Baby (Official Audio Video) [HD].5b814813.mp3"
+  },
+  {
+    "revision": "05d8d65c79dfa9030f75c36ff68b60d0",
+    "url": "/react-music-world/static/media/Forest_Ventures.05d8d65c.mp3"
+  },
+  {
+    "revision": "6bfd74b4e4d1e2258fa9e75bd10bea84",
+    "url": "/react-music-world/static/media/Happy_Dreams.6bfd74b4.mp3"
+  },
+  {
+    "revision": "18440e08823f8b121c95204a15b3a085",
+    "url": "/react-music-world/static/media/LibreBaskerville-Regular.18440e08.ttf"
+  },
+  {
+    "revision": "56cb366910aa712773f8f2f7e7f42b9f",
+    "url": "/react-music-world/static/media/Merienda-Regular.56cb3669.ttf"
+  },
+  {
+    "revision": "70fda92429e8a5f2fbaa5f10da118c74",
+    "url": "/react-music-world/static/media/PT_Sans-Web-Regular.70fda924.ttf"
+  },
+  {
+    "revision": "849e67957ea4dd5edb38eedb9d5743f1",
+    "url": "/react-music-world/static/media/Quando-Regular.849e6795.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/react-music-world/static/media/SourceSansPro-Regular.3f482321.ttf"
+  },
+  {
+    "revision": "fc0ec620db671de43fa88703f91192fb",
+    "url": "/react-music-world/static/media/Tears_of_Joy.fc0ec620.mp3"
+  },
+  {
+    "revision": "c3edb863ceb6f59b6ef8d4490086d705",
+    "url": "/react-music-world/static/media/Warm_Light.c3edb863.mp3"
+  },
+  {
+    "revision": "80cd94b202d91d8813215c2a55a9e7ce",
+    "url": "/react-music-world/static/media/daughtry - home.80cd94b2.mp3"
+  },
+  {
+    "revision": "9dc36e9dfbb5ec6f2d7619429a3b2fa8",
+    "url": "/react-music-world/static/media/deadmau5 - Strobe.9dc36e9d.mp3"
+  },
+  {
+    "revision": "884850355362c0d45be007674dd8be0b",
+    "url": "/react-music-world/static/media/music-space.88485035.jpg"
+  },
+  {
+    "revision": "18a84fbd5d993db1d2bf9804ae882786",
+    "url": "/react-music-world/static/media/משה פרץ - אם זאת מלחמה - moshe perez - em zot milhama.18a84fbd.mp3"
+  }
+]);
